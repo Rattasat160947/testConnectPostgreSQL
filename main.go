449ba60afd3e6dd51b797d0eef2a8eb6b663236a
Rattasat160947 <rattasat0624987765@gmail.com>
2025-11-02 19:18:30 +0700

    API Get

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-)
+	"strconv"
 
-func divide(a, b int) (int, error) {
-	if b == 0 {
-		return 0, errors.New("division by zero")
-	}
-	return a / b, nil
+	"github.com/gofiber/fiber/v2"
+)
+type Book struct {
+	ID    int `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
 }
+var books []Book
+
 
 func main() {
-	result, err := divide(10, 0)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println("Result:", result)
+	app := fiber.New()
+
+	books = append(books, Book{ID: 1, Title: "1984", Author: "George"})
+	books = append(books, Book{ID: 2,Title: "To Kill a Mockingbird", Author: "Harper Lee"})
+
+	app.Get("/books", getBooks)
+	app.Get("/books/:id", getBook)
+	
+ 	app.Listen(":8000")
+}
+
+func getBooks(c *fiber.Ctx) error {
+		return c.JSON(books)
+}
+func getBook(c *fiber.Ctx) error {
+		bookID,err := strconv.Atoi(c.Params("id"))
+		if err != nil{
+			return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		}
+		for _, book := range books {
+			if book.ID == bookID {
+				return c.JSON(book)
+			}
+		}
+		return  c.SendStatus(fiber.StatusBadRequest)
 }
